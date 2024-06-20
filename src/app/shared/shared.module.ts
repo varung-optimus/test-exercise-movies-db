9614db53f2d431f48a4704ddf466d508f80b819a
Varun Goel <varungoel0806@gmail.com>
2024-06-20 18:46:20 +0200

    Removed unneeded component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DialogComponent } from './dialog/dialog.component';
-
-
 
 @NgModule({
   declarations: [
-    DialogComponent
   ],
   imports: [
     CommonModule
   ],
-  exports: [DialogComponent]
+  exports: []
 })
 export class SharedModule { }
